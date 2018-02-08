b6d4ecfe49a4c0dd501771f9a520adba4c4034a8
Barry <tg12@users.noreply.github.com>
2018-02-08 15:38:10 +0000

    Update faig.py

--- faig.py
+++ faig.py
@@ -20,11 +20,11 @@ from sklearn.linear_model import LinearRegression
 import sys, os
 
 ########################################################################################################################
-REAL_OR_NO_REAL = 'https://demo-api.ig.com/gateway/deal'
-API_ENDPOINT = "https://demo-api.ig.com/gateway/deal/session"
-API_KEY = '*******************' 
-API_KEY = '*******************'
-data = {"identifier":"*******************","password": "*******************"}
+# REAL_OR_NO_REAL = 'https://demo-api.ig.com/gateway/deal'
+# API_ENDPOINT = "https://demo-api.ig.com/gateway/deal/session"
+# API_KEY = '**************************' 
+# API_KEY = '**************************'
+# data = {"identifier":"**************************","password": "**************************"}
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
@@ -32,10 +32,10 @@ data = {"identifier":"*******************","password": "*******************"}
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
-# REAL_OR_NO_REAL = 'https://api.ig.com/gateway/deal'
-# API_ENDPOINT = "https://api.ig.com/gateway/deal/session"
-# API_KEY = '*******************'
-# data = {"identifier":"*******************","password": "*******************"}
+REAL_OR_NO_REAL = 'https://api.ig.com/gateway/deal'
+API_ENDPOINT = "https://api.ig.com/gateway/deal/session"
+API_KEY = '**************************'
+data = {"identifier":"**************************","password": "**************************"}
 
 headers = {'Content-Type':'application/json; charset=utf-8',
         'Accept':'application/json; charset=utf-8',
@@ -121,19 +121,6 @@ stopDistance_value = "250" #Initial Stop loss, Worked out later per trade
 #ALL EPICS
 epic_ids = ["CS.D.AUDUSD.TODAY.IP", "CS.D.EURCHF.TODAY.IP", "CS.D.EURGBP.TODAY.IP", "CS.D.EURJPY.TODAY.IP", "CS.D.EURUSD.TODAY.IP", "CS.D.GBPEUR.TODAY.IP", "CS.D.GBPUSD.TODAY.IP", "CS.D.USDCAD.TODAY.IP", "CS.D.USDCHF.TODAY.IP", "CS.D.USDJPY.TODAY.IP", "CS.D.CADCHF.TODAY.IP", "CS.D.CADJPY.TODAY.IP", "CS.D.CHFJPY.TODAY.IP", "CS.D.EURCAD.TODAY.IP", "CS.D.EURSGD.TODAY.IP", "CS.D.EURZAR.TODAY.IP", "CS.D.GBPCAD.TODAY.IP", "CS.D.GBPCHF.TODAY.IP", "CS.D.GBPJPY.TODAY.IP", "CS.D.GBPSGD.TODAY.IP", "CS.D.GBPZAR.TODAY.IP", "CS.D.MXNJPY.TODAY.IP", "CS.D.NOKJPY.TODAY.IP", "CS.D.PLNJPY.TODAY.IP", "CS.D.SEKJPY.TODAY.IP", "CS.D.SGDJPY.TODAY.IP", "CS.D.USDSGD.TODAY.IP", "CS.D.USDZAR.TODAY.IP", "CS.D.AUDCAD.TODAY.IP", "CS.D.AUDCHF.TODAY.IP", "CS.D.AUDEUR.TODAY.IP", "CS.D.AUDGBP.TODAY.IP", "CS.D.AUDJPY.TODAY.IP", "CS.D.AUDNZD.TODAY.IP", "CS.D.AUDSGD.TODAY.IP", "CS.D.EURAUD.TODAY.IP", "CS.D.EURNZD.TODAY.IP", "CS.D.GBPAUD.TODAY.IP", "CS.D.GBPNZD.TODAY.IP", "CS.D.NZDAUD.TODAY.IP", "CS.D.NZDCAD.TODAY.IP", "CS.D.NZDCHF.TODAY.IP", "CS.D.NZDEUR.TODAY.IP", "CS.D.NZDGBP.TODAY.IP", "CS.D.NZDJPY.TODAY.IP", "CS.D.NZDUSD.TODAY.IP", "CS.D.CHFHUF.TODAY.IP", "CS.D.EURCZK.TODAY.IP", "CS.D.EURHUF.TODAY.IP", "CS.D.EURILS.TODAY.IP", "CS.D.EURMXN.TODAY.IP", "CS.D.EURPLN.TODAY.IP", "CS.D.EURTRY.TODAY.IP", "CS.D.GBPCZK.TODAY.IP", "CS.D.GBPHUF.TODAY.IP", "CS.D.GBPILS.TODAY.IP", "CS.D.GBPMXN.TODAY.IP", "CS.D.GBPPLN.TODAY.IP", "CS.D.GBPTRY.TODAY.IP", "CS.D.TRYJPY.TODAY.IP", "CS.D.USDCZK.TODAY.IP", "CS.D.USDHUF.TODAY.IP", "CS.D.USDILS.TODAY.IP", "CS.D.USDMXN.TODAY.IP", "CS.D.USDPLN.TODAY.IP", "CS.D.USDTRY.TODAY.IP", "CS.D.CADNOK.TODAY.IP", "CS.D.CHFNOK.TODAY.IP", "CS.D.EURDKK.TODAY.IP", "CS.D.EURNOK.TODAY.IP", "CS.D.EURSEK.TODAY.IP", "CS.D.GBPDKK.TODAY.IP", "CS.D.GBPNOK.TODAY.IP", "CS.D.GBPSEK.TODAY.IP", "CS.D.NOKSEK.TODAY.IP", "CS.D.USDDKK.TODAY.IP", "CS.D.USDNOK.TODAY.IP", "CS.D.USDSEK.TODAY.IP", "CS.D.AUDCNH.TODAY.IP", "CS.D.CADCNH.TODAY.IP", "CS.D.CNHJPY.TODAY.IP", "CS.D.BRLJPY.TODAY.IP", "CS.D.GBPINR.TODAY.IP", "CS.D.USDBRL.TODAY.IP", "CS.D.USDIDR.TODAY.IP", "CS.D.USDINR.TODAY.IP", "CS.D.USDKRW.TODAY.IP", "CS.D.USDMYR.TODAY.IP", "CS.D.USDPHP.TODAY.IP", "CS.D.USDTWD.TODAY.IP", "CS.D.EURCNH.TODAY.IP", "CS.D.sp_EURRUB.TODAY.IP", "CS.D.GBPCNH.TODAY.IP", "CS.D.NZDCNH.TODAY.IP", "CS.D.USDCNH.TODAY.IP", "CS.D.sp_USDRUB.TODAY.IP"]
 #ALL EPICS
-
-##############################################################################################################################################################################
-##############################################################################################################################################################################
-##############################################################################################################################################################################
-##############################################################################################################################################################################
-#SENSIBLE EPIC LIST, This is just the Major FX from IG Index Web Portal
-#SENSIBLE EPIC LIST
-#SENSIBLE EPIC LIST
-#SENSIBLE EPIC LIST
-#epic_ids = ["CS.D.GBPUSD.TODAY.IP","CS.D.EURUSD.TODAY.IP","CS.D.USDJPY.TODAY.IP","CS.D.AUDUSD.TODAY.IP","CS.D.EURGBP.TODAY.IP","CS.D.EURJPY.TODAY.IP", "CS.D.USDCAD.TODAY.IP","CS.D.GBPEUR.TODAY.IP", "CS.D.USDCHF.TODAY.IP","CS.D.EURCHF.TODAY.IP"]
-#SENSIBLE EPIC LIST
-#SENSIBLE EPIC LIST
-#SENSIBLE EPIC LIST
 
 #*******************************************************************
 #*******************************************************************
@@ -569,8 +556,6 @@ for times_round_loop in range(1, 9999):
             y.append(float(high_price))
             #y = High Prices
 
-        
-        
         base_url = REAL_OR_NO_REAL + '/prices/'+ epic_id + '/MINUTE_3/30'
         # Price resolution (MINUTE, MINUTE_2, MINUTE_3, MINUTE_5, MINUTE_10, MINUTE_15, MINUTE_30, HOUR, HOUR_2, HOUR_3, HOUR_4, DAY, WEEK, MONTH)
         auth_r = requests.get(base_url, headers=authenticated_headers)
@@ -581,8 +566,6 @@ for times_round_loop in range(1, 9999):
         # print(auth_r.reason)
         # print (auth_r.text)
         # print ("-----------------DEBUG-----------------")
-        
-        
 
         for i in d['prices']:
             tmp_list = []
@@ -791,10 +774,7 @@ for times_round_loop in range(1, 9999):
         print ("!!DEBUG!! Most likely DO_A_THING Not Set!!")
         print ("-----------------DEBUG-----------------")
         continue
-    
-    # if Tight_Spread == True:
-        # limitDistance_value = "3"
-    
+
     base_url = REAL_OR_NO_REAL + '/positions/otc'
     authenticated_headers = {'Content-Type':'application/json; charset=utf-8',
             'Accept':'application/json; charset=utf-8',
@@ -942,7 +922,7 @@ for times_round_loop in range(1, 9999):
                 
             if elapsed_time > 10800:
                 print ("!!DEBUG!! WARNING: TRADE HAS BEEN OPEN OVER TIME")
-                if float (PROFIT_OR_LOSS) > 0.50: #50p
+                if float (PROFIT_OR_LOSS) > 0:
                     print ("!!DEBUG!! TRADE OPEN OVER TIME AND IN PROFIT")
                     SIZE = size_value
                     ORDER_TYPE = orderType_value
@@ -965,29 +945,29 @@ for times_round_loop in range(1, 9999):
           
             if elapsed_time > 18000:
                 print ("!!DEBUG!! WARNING: TRADE HAS BEEN OPEN OVER 5 HOURS")
-                if -10 <= float (PROFIT_OR_LOSS) <= 0.50:
+                if -9 <= float (PROFIT_OR_LOSS) <= 0:
                     print ("!!DEBUG!! TRADE OPEN OVER 5 HOURS, CUT LOSSES")
                     #ENABLE THIS CODE WHEN HAPPY WITH VALUES
-                    ########################################
-                    # SIZE = size_value
-                    # ORDER_TYPE = orderType_value
-                    # base_url = REAL_OR_NO_REAL + '/positions/otc'
-                    # data = {"dealId":DEAL_ID,"direction":DIRECTION_TO_CLOSE,"size":SIZE,"orderType":ORDER_TYPE}
-                    # #authenticated_headers_delete IS HACKY AF WORK AROUND!! AS PER .... https://labs.ig.com/node/36
-                    # authenticated_headers_delete = {'Content-Type':'application/json; charset=utf-8',
-                    # 'Accept':'application/json; charset=utf-8',
-                    # 'X-IG-API-KEY':API_KEY,
-                    # 'CST':CST_token,
-                    # 'X-SECURITY-TOKEN':x_sec_token,
-                    # '_method':"DELETE"}
-                    # auth_r = requests.post(base_url, data=json.dumps(data), headers=authenticated_headers_delete) 
-                    # #DEBUG
-                    # print(auth_r.status_code)
-                    # print(auth_r.reason)
-                    # print (auth_r.text)
-                    # print ("DEBUG : TIME AND IN PROFIT :- CLOSED")
+                    #######################################
+                    SIZE = size_value
+                    ORDER_TYPE = orderType_value
+                    base_url = REAL_OR_NO_REAL + '/positions/otc'
+                    data = {"dealId":DEAL_ID,"direction":DIRECTION_TO_CLOSE,"size":SIZE,"orderType":ORDER_TYPE}
+                    #authenticated_headers_delete IS HACKY AF WORK AROUND!! AS PER .... https://labs.ig.com/node/36
+                    authenticated_headers_delete = {'Content-Type':'application/json; charset=utf-8',
+                    'Accept':'application/json; charset=utf-8',
+                    'X-IG-API-KEY':API_KEY,
+                    'CST':CST_token,
+                    'X-SECURITY-TOKEN':x_sec_token,
+                    '_method':"DELETE"}
+                    auth_r = requests.post(base_url, data=json.dumps(data), headers=authenticated_headers_delete) 
+                    #DEBUG
+                    print(auth_r.status_code)
+                    print(auth_r.reason)
+                    print (auth_r.text)
+                    print ("DEBUG : TIME AND IN PROFIT :- CLOSED")
             
-            if float (PROFIT_OR_LOSS) > 4 and elapsed_time > 1800:
+            if float (PROFIT_OR_LOSS) > 6 and elapsed_time > 1800:
                 #ENABLE THIS CODE WHEN HAPPY WITH VALUES
                 ########################################
                 SIZE = size_value
