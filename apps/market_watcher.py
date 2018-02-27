31cd8d2876cb5d799423a53e12b4196d93087cfd
chaubilly <31006209+chaubilly@users.noreply.github.com>
2018-02-27 22:06:54 +0800

    Change ranges to inputs, and other refactoring

--- apps/market_watcher.py
+++ apps/market_watcher.py
@@ -28,36 +28,46 @@ class MarketWatcher():
     bid = None
     ask = None
     # Range of price changes and spread to consider trading.
-    min_change = 0.48
-    max_change = 1.9
-    max_spread = 2
+    min_change = None
+    max_change = None
+    min_spread = None
+    max_spread = None
 
-    def __init__(self, client, epics):
+    def __init__(self, client, epics, change_range=(0.48, 1.9), spread_range=(0, 2)):
+
         assert isinstance(epics, list)
+
+        def is_valid_range(x):
+            assert isinstance(x, (tuple, list))
+            assert all([isinstance(i, float) and i >= 0 for i in x])
+            return True
+
+        assert is_valid_range(change_range)
+        assert is_valid_range(spread_range)
+
         self.client = client
         self.epics = epics
+        self.min_change = min(change_range)
+        self.max_change = max(change_range)
+        self.min_spread = min(spread_range)
+        self.max_spread = max(spread_range)
 
     def watch(self):
         '''This is to keep updating the market data until a valid price movement is observed.'''
         while not self.ok:
-            self.epic = self.__get_random_epic_id()
+            self.epic = self.__get_epic_id()
             self.__update_market_data()
-            if self.__price_change_is_in_range():
-                self.__do_if_price_change_in_range()
+            if self.__price_change_is_in_range() and self.__spread_is_in_range():
+                self.ok = True
+                self.__log('Hit')
             else:
-                self.__do_if_price_change_outside_range()
+                self.ok = False
+                self.__log('Pass')
 
-    def __get_random_epic_id(self):
-        '''
-
-        This is to get random epic ID. It must be different from last traded epic ID.
-
-        If previous traded epic ID os not provided, same epic may be chosen.
-
-        '''
+    def __get_epic_id(self):
+        '''This is to get a random epic in list.'''
         random.shuffle(self.epics)
         epic = random.choice(self.epics)
-        sleep(2)
         return epic
 
     def __update_market_data(self):
@@ -75,11 +85,8 @@ class MarketWatcher():
             else:
                 return float(x)
 
-        # Convert percentage change to float. It could be None.
-        if snapshot['percentageChange'] is None:
-            self.percent_change = 0
-        else:
-            self.percent_change = to_float(snapshot['percentageChange'])
+        # Convert percentage change to float.
+        self.percent_change = to_float(snapshot['percentageChange'])
 
 #       # Convert bid ask prices to float.
         self.bid = to_float(snapshot['bid'])
@@ -87,13 +94,13 @@ class MarketWatcher():
 
         # Calculate spread.
         self.spread = self.ask - self.bid
+        assert self.spread >= 0
 
     def __price_change_is_in_range(self):
         '''This is to check if price change is in range.'''
-        x = self.percent_change
-        return (self.min_change < abs(x) < self.max_change)
+        return (self.min_change < abs(self.percent_change) < self.max_change)
 
-    def __do_if_price_change_in_range(self):
+    def __spread_is_in_range(self):
         '''
 
         This is to check if spread is in range.
@@ -104,28 +111,7 @@ class MarketWatcher():
         Spread is 0.8. This is considered a tight spread.
 
         '''
-        if self.data["spread"] > self.max_spread:
-            self.__do_if_spread_larger_than_limit()
-        elif self.spread < self.max_spread:
-            self.__do_if_spread_smaller_than_limit()
-        else:
-            self.__do_if_spread_equal_limit()
-
-    def __do_if_spread_larger_than_limit(self):
-        self.__log('Pass')
-        self.ok = False
-        systime.sleep(2)
-
-    def __do_if_spread_smaller_than_limit(self):
-        self.__log('Hit')
-        self.ok = True
-
-    def __do_if_spread_equal_limit(self):
-        self.__log('Pass')
-        self.ok = False
-
-    def __do_if_price_change_outside_range(self):
-        self.__log('Pass')
+        return (self.min_spread < self.data["spread"] < self.max_spread)
 
     def __log(self, msg):
         logging.info('epic: {epic}, price: {bid}/{ask}, spread: {spread}, price change: {price_change}, percentage change: {percent_change} -> {msg}'.format(msg=msg, epic=self.epic, bid=int(self.bid), ask=int(self.ask), spread=int(self.spread), price_change=round(self.price_change, 2), percent_change=round(self.percent_change, 2)))
